08b29f373bba37cf0b0451987db06be0687c9469
whftf <aycarter2005@gmail.com>
2025-08-16 17:20:41 -0400

    MUST FIX CHILD PROCESS SPAWN ERROR AS WELL AS HANGTIME... Detailed specific aureport, auditctl, and other basic functionality added to script, updated overall looping mechanism and other syntax as well. Such as y|Y  =~ and regex and even testing the systemctl start and enabled for auditd service

--- Log/audit.py
+++ Log/audit.py
@@ -13,20 +13,19 @@ def main():
     run()
     
 def run():
-
+    x = 0
     keywords = []
     
     #Pull user dir
     dir = os.getcwd()
+    print("Checking if proper files exist...")
+    time.sleep(2)
     #Check if file exists from previous script in proper directory
     file = os.path.exists(f"{dir}/loop.txt")
     if file == True:
-        print("Checking if proper files exist...")
-        time.sleep(2)
         print("\n")
         print(f"{dir}/loop.txt exists! Continuing Script As Normal... ")
         time.sleep(2)
-        
         #Open temp file made to read all keywords
         with open(f'{dir}/loop.txt', 'r') as loooop:
             #Loop thru
@@ -39,24 +38,48 @@ def run():
                     print("Words added to proper file!")
                     break
             
-            #For loop for words now found in loop file
-            for keyWords in keywords:
-                keyWords = keyWords.strip()
-                time.sleep(1)
-                print(f"CHECKING KEYWORD : {keyWords}")
-                print("\n \n\n\n")
-                time.sleep(2)
-                ausearch = f"sudo ausearch -i -k {keyWords}"
-                
-                #Subprocess runs a command to standard output ad error to terminal
-                #Optiuon shell=True: Allows shell process to be ran concurrently with live session
-                #Option cap_output: captures output of the command in live term
-                #Option text=True: ASCII output
-                result = subprocess.run(ausearch, shell=True, capture_output=True, text=True)
-                print("RESULT-FROM-KEYWORD:", result.stdout)
-                print("Errors:", result.stderr)
-                
-                time.sleep(4)
+            
+        while True:
+            try:
+                with open(f'{dir}/loop.txt', 'r', encoding='utf-8') as opened:
+                    
+                    clearme = "clear"
+                    
+                    readme = opened.read()
+                    words = readme.split()
+                    
+                    if len(words) > 0:
+                    
+                        #For loop for words now found in loop file
+                        for keyWords in words:
+                            keyWords = keyWords.strip()
+                            time.sleep(1)
+                            print(f"CHECKING KEYWORD : {keyWords}")
+                            print("\n \n\n\n")
+                            time.sleep(2)
+                            ausearch = f"sudo ausearch -i -k {keyWords}"
+                            
+                            #Subprocess runs a command to standard output ad error to terminal
+                            #Optiuon shell=True: Allows shell process to be ran concurrently with live session
+                            #Option cap_output: captures output of the command in live term
+                            #Option text=True: ASCII output
+                            result = subprocess.run(ausearch, shell=True, capture_output=True, text=True)
+                            print("RESULT-FROM-KEYWORD:", result.stdout)
+                            print("Errors:", result.stderr)
+                            
+                            time.sleep(4)
+                            clearmee = subprocess.run(clearme, shell=True, capture_output=True, text=True)
+                            print(clearmee, result.stdout)
+                        
+                    else:
+                        print(f"No keywords added to {dir}/loop.txt to search for.... ")
+                        time.sleep(1)
+                        print("Proceeding on with regular audit check")
+                        break
+                            
+            except Exception as ff:
+                sys.exit(f"{ff}")
+            
 
     else:
         print(f"{dir}/loop.txt is non existent, try running again...")
--- Log/audit.sh
+++ Log/audit.sh
@@ -22,28 +22,124 @@ cat << EOF
 
 EOF
 
+
+PROC=$(ps -ef | grep audit.sh | grep -v grep | awk '{print $2}')
+
+echo -e "Current processes: $procs "
+
+dirr=$(pwd)
+scriptDir=$("$dirr"/audit.py)
+deps() {
+
+
+	if [[ -z "$scriptDir" ]]; then
+		echo -e "Repo for automation script not found...."
+	fi
+
+	##Check for runtime / service enabled for systemctl
+	sudo systemctl list-units --type=service --state=running | grep -q -e "auditd.service" -e "Audit Logging Service"
+	#Check status of grep
+	running=$?
+	sudo systemctl --runtime status | grep -q auditd.service
+	runtime=$?
+
+	
+	if [[ "$running" -eq 0 && "$runtime" -eq 0 ]]; then
+        	echo -e "Auditd services started on boot as well as started...."
+	else
+        	echo -e "Restarting service and potentially adding it to boot now" && sleep 3
+
+		#Simple enable the service
+		sudo systemctl enable auditd.service 
+
+	fi
+
+
+
+}
+
+
+
+
+
+deps
 echo -e "\n Current KeyWords for audit file: \n "
-doas auditctl -l | cut -d ' ' -f 6
+echo -e "\n\n\n"
+sudo auditctl -l | cut -d ' ' -f 6
 echo -e "\n\n\n"
 sleep 3
+clear
 
-doas auditctl -s
+
+current_amount_of_rules=$(sudo wc /etc/audit/audit.rules|cut -f2 -d ' ')
+#Show general auditctl info
+##Delete rules ######Careful with this
+
+if  [ "$current_amount_of_rules" > 6 ]; then
+	read -p "Delete Current auditctl rules? (y/n) " -en 1 deleteme
+	if [[ "$deleteme" =~ y|Y || -z "$deleteme" ]]; then
+		sudo auditctl -D
+		echo -e "Refreshed the current rules..."
+
+
+	else
+
+
+		echo -e "\n\n Keeping related rules..." && sudo cat /etc/audit/audit.rules
+	fi
+
+
+fi 
+
+
+#Delete and show params anyways if wc below 6 (Default startup wc)
+sudo auditctl -s
+sudo auditctl -D
+
+
 sleep 2
-echo -e "\n Live Log Command and Location: doas tail -f /var/log/audit/audit.log \n "
-echo -e "\n Live Log Command and Location: doas tail -f /var/log/messages"
+echo -e "\n Live Log Command and Location: sudo tail -f /var/log/audit/audit.log \n "
+echo -e "\n Live Log Command and Location: sudo tail -f /var/log/messages"
 sleep 2
-sudo rm loop.txt
+rm loop.txt
+
+
+
 read -p "Create Any rule specific to a directory? (y/n): " create
 
-if [ "$create" == "y" ]; then 
+if [[ "$create" =~ "y"|"Y" ]]; then 
 	while true; do
 	read -p "Specify the Directory to watch over here: (Ex: /home/user/Documents) " dir
 	read -p "Specify the KeyWord used to search up the audit log for this directory here: " name
-	doas auditctl -w "$dir" -p wa -k "$name"
-	sleep 1
-	doas auditctl -l | grep "$name"
-	echo "Imported keyword and rule name above here:"
-	sleep 1
+
+
+	#Check for valid directory input (No more then one / and string)
+	#E.G.:  //home -- incorrect     /home --- correct
+	if ! [[ "$name" =~ "//"+ ]]; then
+
+		sudo auditctl -w "$dir" -p rwa -k "$name"
+		sleep 1 && echo -e "\n\n\n"
+		sudo auditctl -l | grep "$name"
+
+	else
+
+		echo -e "Please enter valid directory path..."
+		continue
+
+
+	fi
+
+
+		#Check if executedcorrectly w/ exit status $?
+	if [ $? -eq 0 ]; then
+		echo -e "Rule imported correctly! \n\n\n"
+		echo "Imported keyword and rule name "$name" with directory "$dir" "
+		sleep 1
+		
+	else
+		echo -e "\n Error with audit creation, try again!"
+		continue
+	fi
 	
 	read -p "Continue Adding more Rules/Directories? (y/n): " continue
 
@@ -59,33 +155,98 @@ if [ "$create" == "y" ]; then
 	done
 
 
-	#Check if executedcorrectly w/ exit status $?
-	if [ $? -eq 0 ]; then
-		echo -e "Rule imported correctly!"
-		
-	else
-		echo -e "\n Error with audit creation, try again!"
-		exit 0
-	fi
-
 else
 	echo "Skipping...."
 	sleep 2
 
-	
-fi	
-echo -e "Checking for any logged data... "
+fi
+
 
 #Implement date feature later to check only from specific dates ... 
 
 #Put all specific auditkeywords in a temp file
-doas auditctl -l | cut -d ' ' -f 6 >> loop.txt
-python3 audit.py
-doas ausearch -m avc
-doas ausearch -m ANOM_ADD_ACCT
-doas ausearch -m ANOM_MOD_ACCT
-doas ausearch -m ANOM_LOGIN_FAILURES
+sudo auditctl -l | cut -d ' ' -f 6 >> loop.txt
+
+#Generic strings to check against ausearch
+gen=(
+
+	avc
+	ANOM_ADD_ACCT 
+	ANOM_MOD_ACCT 
+	ANOM_LOGIN_FAILURES
+	chmod_monitor
+	umaskcheck
+	connects
+	accepts
+	writeattempts
+	createfile
+	chown_monitor
+)
+
+
+if python3 audit.py; then
 
 
 
 
+	echo -e "Checking generic ausearch IOC indicators...."
+	sudo auditctl -a exit,always -F arch=b64 -S chmod -k chown_monitor
+	sudo auditctl -a exit,always -F arch=b64 -S chown -k chmod_monitor
+	sudo auditctl -a exit,always -F arch=b64 -S umask -k umaskcheck 
+	sudo auditctl -a exit,always -F arch=b64 -S connect -k connects
+	sudo auditctl -a exit,always -F arch=b64 -S accept -k accepts
+	sudo auditctl -a exit,always -F arch=b64 -S open -perm=w -k writeattempts
+	sudo auditctl -a exit,always -F arch=b64 -S creat -k createfile 
+
+
+	user=$(whoami)
+	#Loop thru indicies 
+	for genn in "${gen[@]}"; do
+		echo -e "Searching Keyword "$genn" now... \n\n\n"
+		sleep 2
+		checkme=$(sudo ausearch -m "$genn" --start today)
+
+		echo "$checkme" >> genaudit.txt 
+		if ! ls -l genaudit.txt| cut -f1 -d ' ' | grep -q ".rw-r-----."; then
+			sudo chown -R "$user":"$user" genaudit.txt && sudo chmod 640 genaudit.txt
+		
+		fi
+		echo -e "Nothing found for "$genn"... Good Sign"
+		
+
+		sleep 3
+		echo -e "\n\n\n\n"
+	
+
+	done
+
+	#Check for generic syscall infofi
+	sudo ausearch -m execve --start today
+
+	#exit
+
+
+fi
+
+#Show small human readable summary of events 
+sudo aureport --summary
+sudo aureport --avc
+sudo aureport --auth  
+sudo aureport --anomaly
+sudo aureport --virt
+
+dir=$(pwd)
+echo -e "$dir"
+findfiles =$((find "$dir"/loop.txt -type f && find "$dir"/genaudit.txt type -f -perms 640))
+
+if "$findfiles"; then
+	rm "$dir"/loop.txt #&& sudo rm "$dir"/genaudit.txt
+
+fi
+
+
+read -p "Clean all procs? " procs
+
+if [[ "$procs" =~ y|Y ]]; then
+	"$PROC" | xargs kill -9
+fi
