414a907dc13873a81ed106be909c93c4e24f511b
whftf <whftf@gmail.com>
2025-09-12 16:26:56 -0700

    Static debugger, and awk for better proc binary finder

--- system-enum/findprocess.sh
+++ system-enum/findprocess.sh
@@ -4,15 +4,26 @@
 #Find local processes ran via user
 read -p "Process Name (Ex: Firefox): " proc
 x=$(whoami)
-pidofthing=$(ps -u "$x" | grep "$proc" | cut -d ' ' -f 4)
+pidofthing=$(ps -u "$x" | grep "$proc" | awk 'NF=1 {print $1}')
 if [ -z "$pidofthing" ]; then
 	sleep 1 
 	"No Output for a PID or process found..."
 	exit 0
 
 else
-	echo "YOUR BINARIES RUN LOCATION:"
-	ls -l /proc/"$pidofthing"/exe
+	loco=$(ls -l /proc/"$pidofthing"/exe | cut -d ' ' -f11)
+	if [ -n "$loco" ]; then
+
+		echo "YOUR BINARIES RUN LOCATION: "$loco" "
+		#Check functions embedded within binary
+		echo -e "Functions within $loco : \n "
+		strings "$loco" | grep -E '^[a-z0-9_]{4,32}\(\)' | awk '{print $1}' | sort | uniq
+	fi
 	echo -e "YOUR BINARIES PID ON THE SYSTEM: \n $pidofthing"
+	echo -e "Capabilities your process utilizes if any... "
+	getpcaps "$pidofthing" && sleep 2
 	exit 0 
 fi
+
+
+
