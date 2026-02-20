9d17b9590b8f67abc86e2277a68ee444ef16abaa
whftf <aycarter2005@gmail.com>
2026-02-19 21:03:59 -0500

    Commit updated changes to both files

--- Log/audit.py
+++ Log/audit.py
@@ -5,24 +5,19 @@
 
 import os, time, sys, subprocess
 
+LOGFILE="/var/log/audit/audit.log"
+#Pull user dir
+dir = os.getcwd()
+LOOP=f"{dir}/loop.txt"
 
-def main():
-    
-    time.sleep(1)
-    print("Running Log Process Now...")
-    run()
     
 def run():
-    x = 0
     keywords = []
-    
-    #Pull user dir
-    dir = os.getcwd()
     print("Checking if proper files exist...")
     time.sleep(2)
     #Check if file exists from previous script in proper directory
-    file = os.path.exists(f"{dir}/loop.txt")
-    if file == True:
+    file = os.path.exists(LOOP)
+    if file:
         print("\n")
         print(f"{dir}/loop.txt exists! Continuing Script As Normal... ")
         time.sleep(2)
@@ -38,25 +33,25 @@ def run():
                     print("Words added to proper file!")
                     break
             
+        try:
+            with open(f'{dir}/loop.txt', 'r', encoding='utf-8') as opened:
             
-        while True:
-            try:
-                with open(f'{dir}/loop.txt', 'r', encoding='utf-8') as opened:
-                    
-                    clearme = "clear"
-                    
-                    readme = opened.read()
-                    words = readme.split()
-                    
-                    if len(words) > 0:
-                    
-                        #For loop for words now found in loop file
-                        for keyWords in words:
-                            keyWords = keyWords.strip()
+                clearme = "clear"
+                
+                readme = opened.read()
+                words = readme.split()
+                
+                if len(words) > 0 or words != ' ':
+                    checked = []
+                    for keyWords in words:
+                        keyWords = keyWords.strip()
+                        if keyWords not in checked:
+                            checked.append(keyWords)
                             time.sleep(1)
                             print(f"CHECKING KEYWORD : {keyWords}")
-                            print("\n \n\n\n")
+                            print("\n")
                             time.sleep(2)
+                            ##TODO TODO: MUST PIPE TO GREP -V AUDIT SEARCHES
                             ausearch = f"sudo ausearch -i -k {keyWords}"
                             
                             #Subprocess runs a command to standard output ad error to terminal
@@ -64,26 +59,68 @@ def run():
                             #Option cap_output: captures output of the command in live term
                             #Option text=True: ASCII output
                             result = subprocess.run(ausearch, shell=True, capture_output=True, text=True)
-                            print("RESULT-FROM-KEYWORD:", result.stdout)
+                            
+                            if result.stdout == None:
+                                print("Nothing found from log for %s ...."% (keyWords))
+                            print(result.stdout)
                             print("Errors:", result.stderr)
                             
                             time.sleep(4)
-                            clearmee = subprocess.run(clearme, shell=True, capture_output=True, text=True)
-                            print(clearmee, result.stdout)
+                            input("Input anything on keyboard to continue loop...")
+                            os.system(clearme)
+                    
+                else:
+                    print(f"No keywords added to {dir}/loop.txt to search for.... ")
+                    time.sleep(1)
+                    print("Proceeding on with regular audit check")
+                    
                         
-                    else:
-                        print(f"No keywords added to {dir}/loop.txt to search for.... ")
-                        time.sleep(1)
-                        print("Proceeding on with regular audit check")
-                        break
-                            
-            except Exception as ff:
-                sys.exit(f"{ff}")
+        except Exception as ff:
+            sys.exit(f"{ff}")
             
 
     else:
         print(f"{dir}/loop.txt is non existent, try running again...")
         sys.exit("Bye :( ")
+
+
+
+def parseKeywords():
+    proctile = []
+    cwd = []
+    dirExe = []
+    uid = []
+    username = []
+    time = []
+    syscall = []
+    if os.path.exists(LOGFILE):
+        comm = "sudo /usr/sbin/auditctl -l"
+        try:
+            rules = subprocess.run(comm, text=True, capture_output=True, shell=True, check=True)
+            print(rules.stdout, rules.stderr)
+        
+        except subprocess.CalledProcessError:
+           pass
+       
+        if os.path.exists(LOOP):
+                with open(LOGFILE, 'r') as logParse:
+                    logRead = logParse.read().split()
+                    for findKey in logRead:
+                        print(findKey.strip())
+    
+    
+    else:
+        sys.exit("Dedicated log file for auditd does NOT exist? Logging to rsyslog? ")
+
+
+
+
+def main():
+    
+    time.sleep(1)
+    print("Running Log Process Now...")
+    run()
+
 
 
 if __name__ == "__main__":
--- Log/audit.sh
+++ Log/audit.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
+#Script by whftf, auditd framework by ....
 
 #Script For ausearch, auditctl and the audit suite! For Logging specific tasks
 #And Directories within Directories
@@ -23,34 +24,39 @@ cat << EOF
 EOF
 
 
+
+checkroot=$(echo "$UID")
+if [ "$checkroot" -ne 0 ]; then
+	echo "please run as root" && exit 1
+fi
+
+
+
+##### EASY TODO: Implement logrotate feature to auto dump config for user
+
+
 PROC=$(ps -ef | grep audit.sh | grep -v grep | awk '{print $2}')
-
 echo -e "Current processes: $procs "
-
 dirr=$(pwd)
-scriptDir=$("$dirr"/audit.py)
+scriptDir=$(file "$dirr"/audit.py)
 deps() {
-
-
 	if [[ -z "$scriptDir" ]]; then
 		echo -e "Repo for automation script not found...."
 	fi
-
 	##Check for runtime / service enabled for systemctl
-	sudo systemctl list-units --type=service --state=running | grep -q -e "auditd.service" -e "Audit Logging Service"
+	systemctl list-units --type=service --state=running | grep -q -e "auditd.service" -e "Audit Logging Service"
 	#Check status of grep
 	running=$?
-	sudo systemctl --runtime status | grep -q auditd.service
+	systemctl --runtime status | grep -q auditd.service
 	runtime=$?
-
 	
 	if [[ "$running" -eq 0 && "$runtime" -eq 0 ]]; then
         	echo -e "Auditd services started on boot as well as started...."
 	else
-        	echo -e "Restarting service and potentially adding it to boot now" && sleep 3
+        	echo -e "Restarting service and potentially adding it to boot now" && sleep 1
 
 		#Simple enable the service
-		sudo systemctl enable auditd.service 
+		systemctl enable auditd.service 
 
 	fi
 
@@ -63,49 +69,45 @@ deps() {
 
 
 deps
-echo -e "\n Current KeyWords for audit file: \n "
-echo -e "\n\n\n"
-sudo auditctl -l | cut -d ' ' -f 6
-echo -e "\n\n\n"
-sleep 3
-clear
+echo -e "Current KeyWords for audit file:"
+auditctl -l | awk '{print $6}'
+echo -e "\n"
+sleep 1
 
 
-current_amount_of_rules=$(sudo wc /etc/audit/audit.rules|cut -f2 -d ' ')
+current_amount_of_rules=$(wc /etc/audit/audit.rules|awk '{print $2}')
 #Show general auditctl info
 ##Delete rules ######Careful with this
 
-if  [ "$current_amount_of_rules" > 6 ]; then
+if  (( "$current_amount_of_rules" > 6 )); then
 	read -p "Delete Current auditctl rules? (y/n) " -en 1 deleteme
 	if [[ "$deleteme" =~ y|Y || -z "$deleteme" ]]; then
-		sudo auditctl -D
+		auditctl -D
 		echo -e "Refreshed the current rules..."
 
 
 	else
 
 
-		echo -e "\n\n Keeping related rules..." && sudo cat /etc/audit/audit.rules
+		echo -e "\n\n Keeping related rules..." && cat /etc/audit/audit.rules
 	fi
 
 
 fi 
 
-
-#Delete and show params anyways if wc below 6 (Default startup wc)
-sudo auditctl -s
-sudo auditctl -D
-
-
-sleep 2
-echo -e "\n Live Log Command and Location: sudo tail -f /var/log/audit/audit.log \n "
-echo -e "\n Live Log Command and Location: sudo tail -f /var/log/messages"
-sleep 2
-rm loop.txt
+echo -e "Current auditd configuration: \n"
+auditctl -s
+sleep 0.5
+echo -e "Live Log Command and Location: sudo tail -f /var/log/audit/audit.log"
+echo -e "Extra Live Log Command and Location: sudo tail -f /var/log/messages & journalctl -f --system"
+sleep 0.5
+if file loop.txt > /dev/null 2>&1; then
+	rm loop.txt > /dev/null 2>&1;
+fi
 
 
 
-read -p "Create Any rule specific to a directory? (y/n): " create
+read -p "Create Any rule specific to a directory? (y/n): " -en 1 create
 
 if [[ "$create" =~ "y"|"Y" ]]; then 
 	while true; do
@@ -117,9 +119,27 @@ if [[ "$create" =~ "y"|"Y" ]]; then
 	#E.G.:  //home -- incorrect     /home --- correct
 	if ! [[ "$name" =~ "//"+ ]]; then
 
-		sudo auditctl -w "$dir" -p rwa -k "$name"
+		if auditctl -w "$dir" -p rwa -k "$name"; then
+		echo -e "Rule imported correctly! \n\n\n"
+		echo "Imported keyword and rule name "$name" with directory "$dir" "
+		sleep 1
+
+		else
+			
+			read -p  "Error with importing rule.... try again? (y/n): " -en 1 tryagainone
+			if [ "$tryagainone" =~ y|Y ]; then 
+			
+				echo -e "Thisis why"
+			else
+				echo -e "Current Rules: $(sudo auditctl -l) " 
+
+			fi
+
+
+
+		fi
 		sleep 1 && echo -e "\n\n\n"
-		sudo auditctl -l | grep "$name"
+		auditctl -l | grep "$name"
 
 	else
 
@@ -129,25 +149,14 @@ if [[ "$create" =~ "y"|"Y" ]]; then
 
 	fi
 
-
-		#Check if executedcorrectly w/ exit status $?
-	if [ $? -eq 0 ]; then
-		echo -e "Rule imported correctly! \n\n\n"
-		echo "Imported keyword and rule name "$name" with directory "$dir" "
-		sleep 1
-		
-	else
-		echo -e "\n Error with audit creation, try again!"
-		continue
-	fi
-	
-	read -p "Continue Adding more Rules/Directories? (y/n): " continue
+	read -p "Continue Adding more Rules/Directories? (y/n): " -en 1 continue
 
 	if [ "$continue" == "y" ]; then 
 		echo "Creating Another Rule...."
 		continue
 
 	else
+
 
 		break
 	fi
@@ -157,15 +166,127 @@ if [[ "$create" =~ "y"|"Y" ]]; then
 
 else
 	echo "Skipping...."
-	sleep 2
+	sleep 1
 
 fi
 
 
 #Implement date feature later to check only from specific dates ... 
 
+auditbin=$(which auditctl)
 #Put all specific auditkeywords in a temp file
-sudo auditctl -l | cut -d ' ' -f 6 >> loop.txt
+"$auditbin" -l | awk '{print $6}' >> loop.txt
+
+
+
+##CORE UTILS AND RULES SOURCED FROM: https://github.com/Neo23x0/auditd/blob/master/audit.rules
+RULELOKO="/etc/audit/rules.d/audit.rules"
+TEMPUT="$(pwd)/auditnew.rules"
+read -p "OPTIONAL... Create new auditd audit.rules to go by? USES Neo23x0 auditd.rules and other custom ones such as tuning down noise; (y/n) " -en 1 addNeo 
+#Move to newline after input
+echo
+if [[ "$addNeo" =~ y|Y ]]; then
+	echo -e "Generating core audit.rules file..."
+	touch "$TEMPUT"
+
+
+	cat <<EOF >> "$TEMPUT"
+## Increase the buffers to survive stress events.
+## Make this bigger for busy systems
+-b 8192
+
+## This determine how long to wait in burst of events
+--backlog_wait_time 60000
+
+## Set failure mode to syslog
+-f 1
+
+##Attempt to ignore execve syscall from Auditd when writing rules
+-w /etc/sysctl.conf -p wa -k sysctl
+-w /etc/sysctl.d -p wa -k sysctl
+## Stunnel
+-w /usr/sbin/stunnel -p x -k stunnel
+-w /usr/bin/stunnel -p x -k stunnel
+## Cron configuration & scheduled jobs
+-w /etc/cron.allow -p wa -k cron
+-w /etc/cron.deny -p wa -k cron
+-w /etc/cron.d/ -p wa -k cron
+-w /etc/cron.daily/ -p wa -k cron
+-w /etc/cron.hourly/ -p wa -k cron
+-w /etc/cron.monthly/ -p wa -k cron
+-w /etc/cron.weekly/ -p wa -k cron
+-w /etc/crontab -p wa -k cron
+-w /var/spool/cron/ -p wa -k cron
+## Added to catch netcat on Ubuntu
+-w /bin/nc.openbsd -p x -k susp_activity
+-w /bin/nc.traditional -p x -k susp_activity
+## Sbin suspicious activity
+-w /sbin/iptables -p x -k sbin_susp
+-w /sbin/ip6tables -p x -k sbin_susp
+-w /sbin/ifconfig -p x -k sbin_susp
+-w /usr/sbin/arptables -p x -k sbin_susp
+-w /usr/sbin/ebtables -p x -k sbin_susp
+-w /sbin/xtables-nft-multi -p x -k sbin_susp
+-w /usr/sbin/nft -p x -k sbin_susp
+-w /usr/sbin/tcpdump -p x -k sbin_susp
+-w /usr/sbin/traceroute -p x -k sbin_susp
+-w /usr/sbin/ufw -p x -k sbin_susp
+## Suspicious shells
+-w /bin/ash -p x -k susp_shell
+-w /bin/csh -p x -k susp_shell
+-w /bin/fish -p x -k susp_shell
+-w /bin/tcsh -p x -k susp_shell
+-w /bin/tclsh -p x -k susp_shell
+-w /bin/xonsh -p x -k susp_shell
+-w /usr/local/bin/xonsh -p x -k susp_shell
+-w /bin/open -p x -k susp_shell
+-w /bin/rbash -p x -k susp_shell
+## Pam configuration
+-w /etc/pam.d/ -p wa -k pam
+-w /etc/security/limits.conf -p wa  -k pam
+-w /etc/security/limits.d -p wa  -k pam
+-w /etc/security/pam_env.conf -p wa -k pam
+-w /etc/security/namespace.conf -p wa -k pam
+-w /etc/security/namespace.d -p wa -k pam
+-w /etc/security/namespace.init -p wa -k pam
+##SysCall Rules!
+-a always,exit -F arch=b64 -F exe!=/usr/sbin/auditctl -S chmod -k chown_monitor
+-a always,exit -F arch=b64 -F exe!=/usr/sbin/auditctl -S chown -k chmod_monitor
+-a always,exit -F arch=b64 -F exe!=/usr/sbin/auditctl -S umask -k umaskcheck
+-a always,exit -F arch=b64 -F exe!=/usr/sbin/auditctl -S connect -k connects
+-a always,exit -F arch=b64 -S accept -k accepts
+-a always,exit -F arch=b64 -S open -perm=w -k writeattempts
+-a always,exit -F arch=b64 -S creat -k createfile
+-a always,exit -F arch=b64 -S mount -S umount2 -F auid!=-1 -k mount
+-a always,exit -F arch=b64 -S mknod -S mknodat -k specialfiles
+-a always,exit -F arch=b64 -S sethostname -S setdomainname -k network_modifications
+-a always,exit -F arch=b64 -S 44 -F a2=16 -F success=1 -F key=network_connect_4
+-a always,exit -F arch=b64 -F euid=0 -F auid>=1000 -F auid!=-1 -S execve -k rootcmd
+-a always,exit -F arch=b64 -S rmdir -S unlink -S unlinkat -S rename -S renameat -F auid>=1000 -F auid!=-1 -k delete
+
+EOF
+echo
+	sleep 4
+	cat /etc/audit/rules.d/audit.rules | grep -v "#" |  awk '{print $6}' | uniq >> loop.txt
+	echo -e "Generating backup of current audit.rules"
+	if scp /etc/audit/rules.d/audit.rules /etc/audit/rules.d/auditrules"$(date)".BAK; then
+		ls -lah auditnew.rules && cat auditnew.rules
+		mv auditnew.rules /etc/audit/rules.d/audit.rules && systemctl restart auditd 
+		echo -e "\n\n" && sleep 1
+		echo -e "New Default Rules: " && auditctl -l
+
+
+	else
+		echo -e "Copying of file to auditd directory failed... checck directory permissions: "
+		stat /etc/audit
+
+	fi
+
+else
+	echo -e "Skipping..." && sleep 1
+
+fi
+
 
 #Generic strings to check against ausearch
 gen=(
@@ -174,55 +295,46 @@ gen=(
 	ANOM_ADD_ACCT 
 	ANOM_MOD_ACCT 
 	ANOM_LOGIN_FAILURES
-	chmod_monitor
-	umaskcheck
-	connects
-	accepts
-	writeattempts
-	createfile
-	chown_monitor
+	CRED_DISP
+	USER_END
+	BPF
+	NETFILTER_CFG
+	USER_AVC
 )
 
 
+
 if python3 audit.py; then
-
-
-
-
-	echo -e "Checking generic ausearch IOC indicators...."
-	sudo auditctl -a exit,always -F arch=b64 -S chmod -k chown_monitor
-	sudo auditctl -a exit,always -F arch=b64 -S chown -k chmod_monitor
-	sudo auditctl -a exit,always -F arch=b64 -S umask -k umaskcheck 
-	sudo auditctl -a exit,always -F arch=b64 -S connect -k connects
-	sudo auditctl -a exit,always -F arch=b64 -S accept -k accepts
-	sudo auditctl -a exit,always -F arch=b64 -S open -perm=w -k writeattempts
-	sudo auditctl -a exit,always -F arch=b64 -S creat -k createfile 
 
 
 	user=$(whoami)
 	#Loop thru indicies 
 	for genn in "${gen[@]}"; do
 		echo -e "Searching Keyword "$genn" now... \n\n\n"
-		sleep 2
+		sleep 1
+		#Reference each keyword with ausearch framework
 		checkme=$(sudo ausearch -m "$genn" --start today)
 
 		echo "$checkme" >> genaudit.txt 
-		if ! ls -l genaudit.txt| cut -f1 -d ' ' | grep -q ".rw-r-----."; then
+		if ! ls -l genaudit.txt| awk '{print $1}' | grep -q ".rw-r-----."; then
 			sudo chown -R "$user":"$user" genaudit.txt && sudo chmod 640 genaudit.txt
 		
 		fi
 		echo -e "Nothing found for "$genn"... Good Sign"
 		
 
-		sleep 3
+		sleep 1
 		echo -e "\n\n\n\n"
 	
 
 	done
 
-	#Check for generic syscall infofi
-	sudo ausearch -m execve --start today
+	#Check for all syscalls
+	ausearch -m execve --start today
 
+
+	##Check for recent network connections produced via cli:
+	ausearch -k network_connect_4 | grep -v snapd | grep -v ssh | grep -v auditctl | grep "comm"
 	#exit
 
 
@@ -236,13 +348,7 @@ sudo aureport --anomaly
 sudo aureport --virt
 
 dir=$(pwd)
-echo -e "$dir"
-findfiles =$((find "$dir"/loop.txt -type f && find "$dir"/genaudit.txt type -f -perms 640))
-
-if "$findfiles"; then
-	rm "$dir"/loop.txt #&& sudo rm "$dir"/genaudit.txt
-
-fi
+rm "$dir"/loop.txt
 
 
 read -p "Clean all procs? " procs
